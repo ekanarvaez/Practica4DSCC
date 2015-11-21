45a8b5de6da5595dc027c4220793eaaff35f9e81
Federico Domínguez <fexadom@gmail.com>
2015-11-20 19:24:26 -0500

    Agregando archivos iniciales

---
+++ Practica4DSCC/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+//Estas referencias son necesarias para usar GLIDE
+using GHI.Glide;
+using GHI.Glide.Display;
+using GHI.Glide.UI;
+
+namespace Practica4DSCC
+{
+    public partial class Program
+    {
+        //Objetos de interface gráfica GLIDE
+        private GHI.Glide.Display.Window iniciarWindow;
+        private Button btn_inicio;
+
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            /*******************************************************************************************
+            Modules added in the Program.gadgeteer designer view are used by typing 
+            their name followed by a period, e.g.  button.  or  camera.
+            
+            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
+                button.ButtonPressed +=<tab><tab>
+            
+            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
+                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
+                timer.Tick +=<tab><tab>
+                timer.Start();
+            *******************************************************************************************/
+
+
+            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            Debug.Print("Program Started");
+
+            //Carga la ventana principal
+            iniciarWindow = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.inicioWindow));
+            GlideTouch.Initialize();
+
+            //Inicializa el boton en la interface
+            btn_inicio = (Button)iniciarWindow.GetChildByName("button_iniciar");
+            btn_inicio.TapEvent += btn_inicio_TapEvent;
+
+            //Selecciona iniciarWindow como la ventana de inicio
+            Glide.MainWindow = iniciarWindow;
+        }
+
+        void btn_inicio_TapEvent(object sender)
+        {
+            Debug.Print("Iniciar");
+        }
+    }
+}
